9d0eb4d4f7012f751e16d3933c506dbbfb28a966
summer <summer.sun@remowireless.com>
2018-06-13 16:03:55 +0800

    the calculate week is error, modify the calendar.c

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # perpetual-calendar
 A simple code about calendar
+
+Source use Kim larsson calculation formula to calculate week.
+   Kim larsson calculation formula:
+         W= (d+2*m+3*(m+1)/5+y+y/4-y/100+y/400) mod 7
+    W is the week,d is the day,m is the month, y is year.But,if date actually is January and February,the m=13 and 14,as the same time,the year--;
+    for example,if date is 2004-1-10,the y=2003,m=13,d=10(2013-13-10)
+    把一月和二月看成是上一年的十三月和十四月，例：如果是2004-1-10则换算成：2003-13-10来代入公式计算。
--- src/calendar.c
+++ src/calendar.c
@@ -10,43 +10,43 @@ int commond_days[12]={31,28,31,30,31,30,31,31,30,31,30,31};
 
 int Is_Leap_Year(int year)
 {
-	if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-		return true;
-	else 
-		return false;
+   if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+      return true;
+   else 
+      return false;
 }
 
 int Is_Valid_Date(int year, int month, int day)
 {
-	if (year <= 0 || month > 12 || month <= 0 || day <= 0)
-	{
-		return false;
-	}
-	
-	if (true == Is_Leap_Year(year))
-	{
-		if(leap_days[month-1] < day)
-			return false;
-	}
-	else
-	{
-		if(commond_days[month-1] < day)
-			return false;
-	}
-	
-	return true;
+   if (year <= 0 || month > 12 || month <= 0 || day <= 0)
+   {
+      return false;
+   }
+   
+   if (true == Is_Leap_Year(year))
+   {
+      if(leap_days[month-1] < day)
+         return false;
+   }
+   else
+   {
+      if(commond_days[month-1] < day)
+         return false;
+   }
+   
+   return true;
 }
 
 int Get_Days_InMonth(int year, int month)
 {
-	if (year <= 0 || month > 12 || month <= 0)
-	{
-		return false;
-	}
-	if (true == Is_Leap_Year(year))
-		return leap_days[month-1];
-	else
-		return commond_days[month-1];
+   if (year <= 0 || month > 12 || month <= 0)
+   {
+      return false;
+   }
+   if (true == Is_Leap_Year(year))
+      return leap_days[month-1];
+   else
+      return commond_days[month-1];
 }
 
 /**
@@ -57,72 +57,78 @@ int Get_Days_InMonth(int year, int month)
   */
 int Calculation_Week(int year, int month, int day)
 {
-	int ret;
-	int week;
-	ret = Is_Valid_Date(year, month, day);
-	if (-1 == ret)
-	{
-		return -1;
-	}
-	
-	week = (day+2*month+3*(month+1)/5+year+year/4-year/100+year/400) % 7;
-	
-	return week;
+   int ret;
+   int week;
+   ret = Is_Valid_Date(year, month, day);
+   if (-1 == ret)
+   {
+      return -1;
+   }
+   
+   if(month <= 2)
+   {
+      month += 12;
+      year--;
+   }
+   
+   week = (day+2*month+3*(month+1)/5+year+year/4-year/100+year/400) % 7;
+   
+   return week;
 }
 
 int Achieve_Calendar(int date[6][7], int year, int month)
 {
-	int week,days_in_month;
-	int i,j;
-	int day = 1;
-	
-	week = Calculation_Week(year, month, 1);
-	if (-1 == week)
-		return false;
-	week = (week + 1) % 7;
-	days_in_month = Get_Days_InMonth(year,month);
-	if (false == days_in_month)
-		return false;
-		
-	for(i = 0; i < week; i++)
-	{
-		date[0][i] = 0;
-	}
-	for(; i < 7; i++)
-	{
-		date[0][i] = day++;
-	}
-	
-	for(i = 1; i < 6; i++)
-	{
-		for(j = 0; j < 7; j++)
-		{
-			if (day <= days_in_month)
-				date[i][j] = day++;
-			else
-				date[i][j] = 0;
-		}
-	}
-	
-	return true;
+   int week,days_in_month;
+   int i,j;
+   int day = 1;
+   
+   week = Calculation_Week(year, month, 1);
+   if (-1 == week)
+      return false;
+   week = (week + 1) % 7;
+   days_in_month = Get_Days_InMonth(year,month);
+   if (false == days_in_month)
+      return false;
+      
+   for(i = 0; i < week; i++)
+   {
+      date[0][i] = 0;
+   }
+   for(; i < 7; i++)
+   {
+      date[0][i] = day++;
+   }
+   
+   for(i = 1; i < 6; i++)
+   {
+      for(j = 0; j < 7; j++)
+      {
+         if (day <= days_in_month)
+            date[i][j] = day++;
+         else
+            date[i][j] = 0;
+      }
+   }
+   
+   return true;
 }
 
 void Print_Calendar(int date[6][7], int year, int month)
 {
-	int i,j;
-	
-	printf("   %d/%d\n",month,year);
-	printf("SUN  MON  TUE  WEN  THU  FRI  STA\n");
-	
-	for(i = 0; i < 6; i++)
-	{
-		for(j = 0; j < 7; j++)
-		{
-			if (0 != date[i][j])
-				printf("%2d   ",date[i][j]);
-			else
-				printf("     ");
-		}
-		printf("\n");
-	}
+   int i,j;
+   
+   printf("   %d/%d\n",month,year);
+   printf("SUN  MON  TUE  WEN  THU  FRI  STA\n");
+   
+   for(i = 0; i < 6; i++)
+   {
+      for(j = 0; j < 7; j++)
+      {
+         if (0 != date[i][j])
+            printf("%2d   ",date[i][j]);
+         else
+            printf("     ");
+      }
+      printf("\n");
+   }
 }
